a4c5d57082ad148e365ebf14f9f5a4ecd4818734
soar1219 <tsubasa1219.info@gmail.com>
2023-09-27 17:40:21 +0900

    レンダリング確認用のコーディング

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { useState } from 'react';
 import './style.css';
+import { ChildArea } from './ChildArea';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
 
-  const onClickCountUp = () => {
-    setCount(count + 1);
+  const onChangeText = (e) => {
+    setText(e.target.value)
   }
 
+  const onClickOpen = () => {
+    setOpen(!open)
+  }
+
+
+
   return (
-    <>
-      <p>{count}</p>
-      <button onClick={onClickCountUp}>カウントアップ</button>
-    </>
+    <div className='App'>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
+    </div>
   );
 }
 
