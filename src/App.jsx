3ce0101b8748d12ae4538a43778177b6e5c2f318
soar1219 <tsubasa1219.info@gmail.com>
2023-10-02 17:38:24 +0900

    git status

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { InlineStyle } from "./components/InlineStyle";
 import { CssModules } from "./components/CssModules";
+import { StyledJsx } from "./components/StyledJsx";
+import { StyledComponents } from "./components/StyledComponents";
 
 function App() {
     return(
         <>
             <InlineStyle />
             <CssModules />
+            <StyledJsx />
+            <StyledComponents />
         </>
     )
 }
