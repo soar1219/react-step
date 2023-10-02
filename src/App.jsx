c1ce947351ffb885e51728521b29cd8dd9d17305
soar1219 <tsubasa1219.info@gmail.com>
2023-10-02 17:07:31 +0900

    CSSモジュールを使ったスタイル設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { InlineStyle } from "./components/InlineStyle";
+import { CssModules } from "./components/CssModules";
 
 function App() {
     return(
-        <InlineStyle />
+        <>
+            <InlineStyle />
+            <CssModules />
+        </>
     )
 }
 
